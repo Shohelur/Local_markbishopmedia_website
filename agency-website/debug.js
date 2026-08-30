212324d7515cf32090d29f506bb78d2525b0c821
Mark Bishop Media <dev@markbishopmedia.com>
2026-08-30 23:27:17 +0600

    Clean up temporary AI injection and layout fix scripts from root directory

--- agency-website/debug.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('final-website-master.html', 'utf8');
-const start = html.indexOf('<div class="radar-ui-container"');
-console.log(html.substring(start, start + 2500));
--- agency-website/test-inspect.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-// We will inspect how the layout renders under different viewport sizes
-console.log("Visual Inspection Script Ready");
